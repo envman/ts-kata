dfd252bdc851a9c5910e8e5fbe467cca99eae8b4
James Birkett <james.birkett@land.tech>
2024-07-10 16:15:59 +0100

    renamed tests for clarity

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from "@jest/globals";
+import { describe, it, expect } from "@jest/globals";
 
 export function add(input: string) {
   let delimiter = ",";
@@ -19,65 +19,65 @@ export function add(input: string) {
 }
 
 describe("add", () => {
-  test("an empty string returns 0", () => {
+  it("accepts an empty string and returns 0", () => {
     const expected = 0;
     const result = add("");
     expect(result).toEqual(expected);
   });
-  test("'1' returns 1", () => {
+  it("accepts an string with a value of '1' and returns that as a number", () => {
     const expected = 1;
     const result = add("1");
     expect(result).toEqual(expected);
   });
-  test("'2' returns 2", () => {
+  it("accepts other numeric values as a string and returns them as a number", () => {
     const expected = 2;
     const result = add("2");
     expect(result).toEqual(expected);
   });
-  test("'1,1' returns 2", () => {
+  it("takes a pair of comma-separated numbers and returns their sum", () => {
     const expected = 2;
     const result = add("1,1");
     expect(result).toEqual(expected);
   });
-  test("'1,1,1' returns 3", () => {
+  it("takes any number of comma-separated numbers and returns their sum", () => {
     const expected = 3;
     const result = add("1,1,1");
     expect(result).toEqual(expected);
   });
-  test("'1\n1,1' returns 3", () => {
-    const expected = 3;
-    const result = add("1\n1,1");
-    expect(result).toEqual(expected);
-  });
-  test("'1\n1\n1' returns 3", () => {
+  it("takes a sequence of numbers separated by a newline and returns their sum", () => {
     const expected = 3;
     const result = add("1\n1\n1");
     expect(result).toEqual(expected);
   });
-  test("'1\n1\n1\n1\n1' returns 5", () => {
+  it("takes a sequence of numbers separated either by a comma or a newline and returns their sum", () => {
+    const expected = 3;
+    const result = add("1\n1,1");
+    expect(result).toEqual(expected);
+  });
+  it("takes a longer sequence of numbers separated either by a newline and returns their sum", () => {
     const expected = 5;
     const result = add("1\n1\n1\n1\n1");
     expect(result).toEqual(expected);
   });
-  test("'//,\n1,1' returns 2", () => {
+  it("can take a specified delimiter of a comma, prefixed by // and followed by a newline and returns the sum of numbers ", () => {
     const expected = 2;
     const result = add("//,\n1,1");
     expect(result).toEqual(expected);
   });
-  test("'//,\n' returns 0", () => {
+  it("will default to returning if no numbers are specified after the delimiter", () => {
     const expected = 0;
     const result = add("//,\n");
     expect(result).toEqual(expected);
   });
-  test("'//;\n1;2' returns 3", () => {
+  it("will return the sum of numbers if the delimiter is a character other than a comma", () => {
     const expected = 3;
     const result = add("//;\n1;2");
     expect(result).toEqual(expected);
   });
-  test("throws error for negative inputs", () => {
+  it("throws error for negative inputs", () => {
     expect(() => add("3,-1")).toThrow(Error);
   });
-  test("Includes negative values in error messages", () => {
+  it("includes any negative values in the error's message", () => {
     expect(() => add("3,-1")).toThrow(/-1/);
   });
 });
