659a0f0a8d9df0179713b3b33a99a44ef2ad1cd1
James Birkett <james.birkett@land.tech>
2024-07-10 16:08:21 +0100

    added test for including negative numbers in error message

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -75,6 +75,9 @@ describe("add", () => {
     expect(result).toEqual(expected);
   });
   test("throws error for negative inputs", () => {
-    expect(() => add("3,-1")).toThrow(/Negatives not allowed!/);
+    expect(() => add("3,-1")).toThrow(Error);
+  });
+  test("Includes negative values in error messages", () => {
+    expect(() => add("3,-1")).toThrow(/-1/);
   });
 });
