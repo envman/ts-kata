ada6c86c513ec434279ed6c5cef266d3a3c85330
Miles Pool <milesjpool@gmail.com>
2024-07-11 12:15:16 +0100

    Add test, and delete unused file.

--- tests/example.test.ts
+++
@@ -1,8 +0,0 @@
-import { example } from "../src/example"
-import { describe, test, expect } from "@jest/globals"
-
-describe('Sum function', () =>{
-    test('Returns correct value', () =>{
-        expect(example(2, 3)).toEqual(5)
-    })
-})
